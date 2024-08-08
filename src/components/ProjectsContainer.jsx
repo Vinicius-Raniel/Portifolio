967c37fb0b34f618f6e37a075fce3769887fda23
vinicius-raniel <vinicinn_raniel10@hotmail.com>
2024-08-08 17:21:48 -0300

    Coreeção de letras

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,7 +3,7 @@ const ProjectsContainer = () => {
       <section className="projects-container">
         <h2>Projetos</h2>
         <p>
-         Todos os meu projetos com o deploy você encontra nas minha publicações no Linkedin!
+         Todos os meu projetos com o deploy você encontra nas minhas publicações no Linkedin!
         </p>
         <a href="https://www.linkedin.com/in/vinicius-raniel/" className="btn">
           Ver Projetos
