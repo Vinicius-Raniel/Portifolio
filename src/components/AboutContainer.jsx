9e04ddee9466bde16174c554551e5edeef870979
vinicius-raniel <vinicinn_raniel10@hotmail.com>
2024-08-08 17:04:37 -0300

    Alteração de letra

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -9,7 +9,7 @@ const AboutContainer = () => {
         <p>
         O DevClub é uma formação de Programadores no qual aprendemos várias linguagens tanto na teoria quanto na prática para 
         programar e nos tornarmos qualificados para o mercado de trabalho.
-        É uma formção completa onde público com 0 conhecimento em programação aprende até ao avançado!
+        É uma formação completa onde público com 0 conhecimento em programação aprende até ao avançado!
         </p>
       </section>
       
