abea000c02bd33a7f3dd51607b3d0dc2d19db4af
vinicius-raniel <vinicinn_raniel10@hotmail.com>
2024-08-08 16:36:55 -0300

    Primeiro Commit

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,19 @@
+const AboutContainer = () => {
+    return (
+      <section className="about-container">
+        <h2>Sobre</h2>
+        <p>
+        Atualmente em transição de carreira saindo de motoboy para se tornar um desenvolvedor de sucesso!
+        Tenho conhecimento Full-Stack e já trabalhei com algumas tecnologias que usei pelo DevClub.
+        </p>
+        <p>
+        O DevClub é uma formação de Programadores no qual aprendemos várias linguagens tanto na teoria quanto na prática para 
+        programar e nos tornarmos qualificados para o mercado de trabalho.
+        É uma formção completa onde público com 0 conhecimento em programação aprende até ao avançado!
+        </p>
+      </section>
+      
+    );
+  };
+  
+  export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,15 @@
+const ProjectsContainer = () => {
+    return (
+      <section className="projects-container">
+        <h2>Projetos</h2>
+        <p>
+         Todos os meu projetos com o deploy você encontra nas minha publicações no Linkedin!
+        </p>
+        <a href="https://www.linkedin.com/in/vinicius-raniel/" className="btn">
+          Ver Projetos
+        </a>
+      </section>
+    );
+  };
+  
+  export default ProjectsContainer;
